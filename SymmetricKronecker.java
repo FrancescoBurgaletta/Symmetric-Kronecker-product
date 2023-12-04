2a0f694e9f03d9d531576138d9c2f3298867ab7f
FrancescoBurgaletta <143550199+FrancescoBurgaletta@users.noreply.github.com>
2023-12-04 18:31:16 +0100

    Update SymmetricKronecker.java

--- SymmetricKronecker.java
+++ SymmetricKronecker.java
@@ -1,30 +1,28 @@
 class SymmetricKronecker {
 
-    public static double[][] symmetric_kronecker_product(double[][] m1, double[][] m2) {
-    	if (m1.length != m1[0].length || m2.length != m2[0].length || m1.length != m2.length) {
-    		System.out.println("The symmetric Kronecker product can be performed only on square matrices of the same size.\n");
-    
-    		return null;
+    public static double[][] symmetric_kronecker_product(double[][] a, double[][] b) throws IllegalArgumentException {
+    	if (a.length != a[0].length || b.length != b[0].length || a.length != b.length) {
+    		throw new IllegalArgumentException("The symmetric Kronecker product can be performed only on square matrices of the same size.");
     	}
 
-    	int resultSize = (m1.length * (m1.length + 1)) / 2;
+    	int resultSize = (a.length * (a.length + 1)) / 2;
     	double[][] product = new double[resultSize][resultSize];
     	int rowQ = 0, divQ = 0, modQ = 0, colT = 0, divT = 0, modT = 0;
     	double inverseSqrt2 = Math.pow(2, -0.5);
 
     	modT = 1;
     	colT = 1;
-    	product[0][0] = m1[0][0] * m2[0][0];
+    	product[0][0] = a[0][0] * b[0][0];
 
-    	while (divT + 1 < m1.length) {
-    		while (modT < m1.length) {
-    			product[0][colT] = (m1[0][divT] * m2[0][modT] + m1[0][modT] * m2[0][divT]) * inverseSqrt2;
+    	while (divT + 1 < a.length) {
+    		while (modT < a.length) {
+    			product[0][colT] = (a[0][divT] * b[0][modT] + a[0][modT] * b[0][divT]) * inverseSqrt2;
     			colT++;
     			modT++;
     		}
 
     		divT++;
-    		product[0][colT] = m1[0][divT] * m2[0][divT];
+    		product[0][colT] = a[0][divT] * b[0][divT];
     		modT = divT + 1;
     		colT++;
     	}
@@ -34,20 +32,20 @@ class SymmetricKronecker {
     	divT = 0;
     	modT = 1;
 
-    	while (divQ + 1 < m1.length) {
-    		while (modQ < m1.length) {
-    			product[rowQ][0] = (m1[divQ][0] * m2[modQ][0] + m1[modQ][0] * m2[divQ][0]) * inverseSqrt2;
+    	while (divQ + 1 < a.length) {
+    		while (modQ < a.length) {
+    			product[rowQ][0] = (a[divQ][0] * b[modQ][0] + a[modQ][0] * b[divQ][0]) * inverseSqrt2;
     			colT = 1;
 
-    			while (divT + 1 < m1.length) {
-    				while (modT < m1.length) {
-    					product[rowQ][colT] = (m1[divQ][divT] * m2[modQ][modT] + m1[modQ][modT] * m2[divQ][divT] + m1[divQ][modT] * m2[modQ][divT] + m1[modQ][divT] * m2[divQ][modT]) * 0.5;
+    			while (divT + 1 < a.length) {
+    				while (modT < a.length) {
+    					product[rowQ][colT] = (a[divQ][divT] * b[modQ][modT] + a[modQ][modT] * b[divQ][divT] + a[divQ][modT] * b[modQ][divT] + a[modQ][divT] * b[divQ][modT]) * 0.5;
     					colT++;
     					modT++;
     				}
 
     				divT++;
-    				product[rowQ][colT] = (m1[divQ][divT] * m2[modQ][divT] + m1[modQ][divT] * m2[divQ][divT]) * inverseSqrt2;
+    				product[rowQ][colT] = (a[divQ][divT] * b[modQ][divT] + a[modQ][divT] * b[divQ][divT]) * inverseSqrt2;
     				modT = divT + 1;
     				colT++;
     			}
@@ -59,19 +57,19 @@ class SymmetricKronecker {
     		}
 
     		divQ++;
-    		product[rowQ][0] = m1[divQ][0] * m2[divQ][0];
+    		product[rowQ][0] = a[divQ][0] * b[divQ][0];
     		modQ = divQ + 1;
     		colT = 1;
 
-    		while (divT + 1 < m1.length) {
-    			while (modT < m1.length) {
-    				product[rowQ][colT] = (m1[divQ][divT] * m2[divQ][modT] + m1[divQ][modT] * m2[divQ][divT]) * inverseSqrt2;
+    		while (divT + 1 < a.length) {
+    			while (modT < a.length) {
+    				product[rowQ][colT] = (a[divQ][divT] * b[divQ][modT] + a[divQ][modT] * b[divQ][divT]) * inverseSqrt2;
     				colT++;
     				modT++;
     			}
 
     			divT++;
-    			product[rowQ][colT] = m1[divQ][divT] * m2[divQ][divT];
+    			product[rowQ][colT] = a[divQ][divT] * b[divQ][divT];
     			modT = divT + 1;
     			colT++;
     		}
